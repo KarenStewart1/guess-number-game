db26528a3dc9d92718b728dbf53916ff0061b4a4
Karen Stewart <karenstewart1117@gmail.com>
2022-01-14 20:17:29 +0100

    add ability to compare user input to computer generated number and change the message displayed depending on that comparison

--- src/CheckGuess.js
+++ src/CheckGuess.js
@@ -2,7 +2,25 @@ import React, { useState } from "react";
 import "./CheckGuess.css";
 
 export default function CheckGuess(props) {
+  // if computer number is the same = the computer number/my number is revealed in the box, the screen turns green,
+  //  regenerate random number when again button presses
+  // highscore is saved through multiple games
   const [guess, setGuess] = useState(null);
+  const [currentScore, setCurrentScore] = useState(20);
+  const [message, setMessage] = useState("Start guessing...");
+  function compareNumbers() {
+    if (guess === props.randomNumber) {
+      setMessage("Correct!");
+    } else if (guess < 1) {
+      setMessage("Please guess a number between 1 and 20");
+    } else if (guess > 20) {
+      setMessage("Please guess a number between 1 and 20");
+    } else if (guess < props.randomNumber) {
+      setMessage("Too low!");
+    } else if (guess > props.randomNumber) {
+      setMessage("Too high!");
+    }
+  }
 
   function updateGuess(event) {
     setGuess(Number(event.target.value));
@@ -10,6 +28,7 @@ export default function CheckGuess(props) {
 
   function handleClick(event) {
     event.preventDefault();
+    compareNumbers();
   }
   return (
     <main className="CheckGuess">
@@ -34,5 +53,6 @@ export default function CheckGuess(props) {
         </p>
       </section>
     </main>
+          <p className="message">{message}</p>
   );
 }
