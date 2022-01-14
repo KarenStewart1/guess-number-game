cea27483e262d9470b5c781f89812091ab48f302
Karen Stewart <karenstewart1117@gmail.com>
2022-01-14 17:01:19 +0100

    add HTML starter code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {" "}
+      <header>
+        <h1>Guess My Number!</h1>
+        <p className="between">(Between 1 and 20)</p>
+        <button className="btn again">Again!</button>
+        <div className="number">?</div>
+      </header>
+      <main>
+        <section className="right">
+          <p className="message">Start guessing...</p>
+          <p className="label-score">
+            <span role="img" aria-label="top score emoji">
+              {" "}
+              💯{" "}
+            </span>
+            Score: <span className="score"></span>
+          </p>
+          <p className="label-highscore">
+            <span role="img" aria-label="gold medal">
+              {" "}
+              🥇
+            </span>{" "}
+            Highscore: <span className="highscore"></span>
+          </p>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
