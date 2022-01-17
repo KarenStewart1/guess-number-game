4ff14bd0b6928042517e3f67ed52cd8ab943c70e
Karen Stewart <karenstewart1117@gmail.com>
2022-01-17 19:31:21 +0100

    refactor code + fix of error when user's score us equal to 1

--- src/App.js
+++ src/App.js
@@ -2,44 +2,46 @@ import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  const [guess, setGuess] = useState(null);
+  const [randomNumber, setRandomNumber] = useState(
+    Math.floor(Math.random() * (Math.floor(20) - Math.ceil(0)) + Math.ceil(1))
+  );
+  const [guess, setGuess] = useState("");
   const [currentScore, setCurrentScore] = useState(20);
   const [highScore, setHighScore] = useState(0);
   const [message, setMessage] = useState("Start guessing...");
   const [reveal, setReveal] = useState("?");
-  const [randomNumber, setRandomNumber] = useState();
 
+
+  function didUserWin(outcome) {
+    setReveal(randomNumber);
+    setMessage(outcome ? "🎉 Correct!" : "💥 You lost!");
+    document.body.style.backgroundColor = outcome ? "#4ef037" : "#dc2f2f";
+    if (outcome && currentScore > highScore) {
+      setHighScore(currentScore);
+    } else if (!outcome) {
+      setCurrentScore(0);
+    }
+  }
+
+  function tooLowOrTooHigh() {
+    setCurrentScore(currentScore - 1);
+    setMessage(guess > randomNumber ? "📈 Too high!" : "📉 Too low!");
   }
 
   function compareNumbers() {
-    if (currentScore > 1) {
-      if (guess < 1) {
-        setMessage("Please guess a number between 1 and 20");
-      } else if (!guess) {
-        setMessage("⛔️ No number!");
-      } else if (guess === randomNumber) {
-        setMessage("🎉 Correct!");
-        setReveal(randomNumber);
-        document.body.style.backgroundColor = "#4ef037";
-        if (currentScore > highScore) {
-          setHighScore(currentScore);
-        }
-      } else if (guess > 20) {
-        setMessage("Please guess a number between 1 and 20");
-      } else if (guess < randomNumber) {
-        setMessage("📉 Too low!");
-        setCurrentScore(currentScore - 1);
-      } else if (guess > randomNumber) {
-        setMessage("📈 Too high!");
-        setCurrentScore(currentScore - 1);
+    if (reveal !== randomNumber) {
+      if (guess < 1 || guess > 20) {
+        setMessage("⛔️ Please guess a number between 1 and 20");
+      } else if (currentScore > 1) {
+        return guess === randomNumber ? didUserWin(true) : tooLowOrTooHigh();
+      } else if (currentScore === 1) {
+        return guess !== randomNumber ? didUserWin(false) : didUserWin(true);
       }
-    } else if (currentScore === 1) {
-      document.body.style.backgroundColor = "#dc2f2f";
-      setMessage("💥 You lost!");
-      setCurrentScore(0);
-      setReveal(randomNumber);
+    } else {
+      setMessage("Please play again!");
     }
   }
+
   function updateGuess(event) {
     setGuess(Number(event.target.value));
   }
@@ -58,57 +60,56 @@ export default function App() {
     document.body.style.backgroundColor = "#222";
   }
 
-    return (
-      <div className="App">
-        <header>
-          <h1>Guess My Number!</h1>
-          <p className="between">(Between 1 and 20)</p>
-          <button className="btn again" onClick={againButton}>
-            Again!
+  return (
+    <div className="App">
+      <header>
+        <h1>Guess My Number!</h1>
+        <p className="between">(Between 1 and 20)</p>
+        <button className="btn again" onClick={againButton}>
+          Again!
+        </button>
+        <div
+          className={
+            reveal === randomNumber ? "number big-box" : "number small-box"
+          }
+        >
+          {reveal}
+        </div>
+      </header>
+      <main>
+        <section className="left">
+          <input type="number" className="guess" onChange={updateGuess} />
+          <button className="btn check" onClick={handleClick}>
+            Guess!
           </button>
-          <div
-            className={
-              reveal == randomNumber ? "number big-box" : "number small-box"
-            }
-          >
-            {reveal}
-          </div>
-        </header>
-        <main>
-          <section className="left">
-            <input type="number" className="guess" onChange={updateGuess} />
-            <button className="btn check" onClick={handleClick}>
-              Guess!
-            </button>
-          </section>
-          <section className="right">
-            <p className="message">{message}</p>
-            <p className="label-score">
-              <span role="img" aria-label="top score emoji">
-                💯
-              </span>
-              Score: <span className="score">{currentScore}</span>
-            </p>
-            <p className="label-highscore">
-              <span role="img" aria-label="gold medal">
-                🥇
-              </span>
-              Highscore: <span className="highscore">{highScore}</span>
-            </p>
-          </section>
-        </main>
-        <footer>
-          This game was coded by{" "}
-          <a
-            href="https://www.karenstewart.nl"
-            className={currentScore == 0 ? "black-text" : "red-text"}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Karen Stewart
-          </a>
-        </footer>
-      </div>
-    );
-  }
+        </section>
+        <section className="right">
+          <p className="message">{message}</p>
+          <p className="label-score">
+            <span role="img" aria-label="top score emoji">
+              💯{" "}
+            </span>
+            Score: <span className="score">{currentScore}</span>
+          </p>
+          <p className="label-highscore">
+            <span role="img" aria-label="gold medal">
+              🥇{" "}
+            </span>
+            Highscore: <span className="highscore">{highScore}</span>
+          </p>
+        </section>
+      </main>
+      <footer>
+        This game was coded by{" "}
+        <a
+          href="https://www.karenstewart.nl"
+          className={currentScore === 0 ? "black-text" : "red-text"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Karen Stewart
+        </a>
+      </footer>
+    </div>
+  );
 }
